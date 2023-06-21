d59a051a949b48dfbb7729274cc555134fa93e90
Inam Emediong <102903638+Inam-E@users.noreply.github.com>
2023-06-21 20:12:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const faqQuestions = document.querySelectorAll(".faq_question");
+
+faqQuestions.forEach(question => {
+    question.addEventListener("click", () => {
+        const faqAnswer = question.nextElementSibling;
+        const allOtherAnswers = document.querySelectorAll(".faq_answer");
+
+        allOtherAnswers.forEach(answer => {
+            if (answer !== faqAnswer) {
+                answer.style.display = "none";
+            }
+        });
+
+        faqAnswer.style.display = faqAnswer.style.display === "none" ? "block" : "none";
+    });
+});
